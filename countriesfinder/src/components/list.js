bd79784c0cba11f5facf11d586e35a49d5b3a505
Anmol <anmolp476@gmail.com>
2022-10-10 01:13:16 -0400

    Tested the data by outputing all the names of the countries as a list

--- countriesfinder/src/components/list.js
+++ countriesfinder/src/components/list.js
@@ -1,18 +1,28 @@
 import React from 'react';
 
 
-const List = (props) =>
+const ListView = (props) =>
 {
+    let dataVar = props.theData;
+    const currSearch = props.searched;
+
+    //console.log(dataVar[0].name.common);
     return(
         <div>
-
+            <ul>
+                <li>{dataVar[0].name.common}</li>
+                {dataVar.map((country) =>
+                {
+                    return <li>{country.name.common}</li>
+                })}
+            </ul>
         </div>
     );
 
 }
 
 
-export default List;
+export default ListView;
 
 
 
