4277608eef906e1d4e13761ccc353df194b74461
Anmol <anmolp476@gmail.com>
2022-10-09 23:49:37 -0400

    Imported the useEffect and useState hook to import the JSON data from the API and set that data to a state respectively

--- countriesfinder/src/App.js
+++ countriesfinder/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import axios from 'axios';
+import {React, useEffect, useState} from 'react';
 import View from './components/view';
 
 
 function App() {
+
+  const [data, setData] = useState();
+
+  useEffect(() =>
+  {
+    axios.get("https://restcountries.com/v3.1/all")
+         .then(response => setData(response.data));
+  })
+
   return (
     <div className="App">
-        <View/>
+        <View countriesData={data}/>
     </div>
   );
 }
