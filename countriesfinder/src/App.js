b4759057f4e0c53da13653503c53102e72858ee7
Anmol <anmolp476@gmail.com>
2022-10-09 23:16:08 -0400

    Created the search bar component

--- countriesfinder/src/App.js
+++ countriesfinder/src/App.js
@@ -1,9 +1,32 @@
+import React from 'react';
+import {useState} from 'react';
+
+
+const SearchBar = ({text}) =>
+{
+  const [search, setSearch] = useState('');
+
+  console.log(search);
+
+  const inputChange = (event) =>
+  {
+    //console.log(event.target.value);
+    setSearch(event.target.value);
+  }
+
+  return(
+    <div>
+      {text}<input value={search} onChange={inputChange}></input>
+    </div>
+  );
+
+}
 
 
 function App() {
   return (
     <div className="App">
-        
+        <SearchBar text="find countries"/>
     </div>
   );
 }
