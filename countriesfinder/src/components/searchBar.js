adb259255c58a1c535f4c32b4f054b065e012fcf
Anmol <anmolp476@gmail.com>
2022-10-09 23:51:34 -0400

    Added a bit of space between the text and the search bar

--- countriesfinder/src/components/searchBar.js
+++ countriesfinder/src/components/searchBar.js
@@ -5,7 +5,7 @@ const SearchBar = (props) =>
 {
   return(
     <div>
-      {props.text}<input value={props.values} onChange={props.change}></input>
+      {props.text} <input value={props.values} onChange={props.change}></input>
     </div>
   );
 }
